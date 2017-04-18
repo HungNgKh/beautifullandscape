4b43fb12b9ddbcfcb289858e31816bb1540a37d1
dat-hedspi <dat.hedspi@gmail.com>
2017-04-18 11:25:03 +0700

    create, show, index image

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :images
-  root 'static_pages#home'
+  resources :posts
+
+  root 'posts#index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
