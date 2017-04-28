512c22dad5e3ca20d9ccc7c1bc4cc565ec075575
“LePhuc” <lephuc@Les-MacBook-Pro.local>
2017-04-28 14:03:59 +0700

    Add comments to post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   root 'posts#index'
-  resources :posts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts do
+    resources :comments
+  end
 end
