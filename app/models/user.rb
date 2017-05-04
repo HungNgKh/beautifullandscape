80faa9468c150e70a729cb8a923ae460a233d27c
“LePhuc” <lephuc@Les-MacBook-Pro.local>
2017-05-04 18:07:18 +0700

    Like post

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,9 @@ class User < ApplicationRecord
   validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
+  def likes?(post)
+    post.likes.where(user_id: id).any?
+  end
 end
